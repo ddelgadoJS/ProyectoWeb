f72a73870c88afe347ef4d539aebab0540c3f135
Daniel Delgado <delgado.jdds@gmail.com>
2019-10-27 12:38:50 -0600

    Registration and login working with Django users

--- venv/src/pages/views.py
+++ venv/src/pages/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def home_view(request, *args, **kwargs):
     return render(request, "home.html", {})
--- venv/src/users/views.py
+++ venv/src/users/views.py
@@ -1,39 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
-from .forms import UserCreateForm, UserLoginForm
+from .forms import UserCreateForm, UserLoginForm, RegistrationForm
 from .models import User
 
 # Create your views here.
 def user_create_view(request):
-    my_form = UserCreateForm()
+    form = RegistrationForm()
     if request.method == "POST":
-        my_form = UserCreateForm(request.POST)
-        if my_form.is_valid():
-            User.objects.create(**my_form.cleaned_data)
-            return HttpResponseRedirect('/login/')
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegistrationForm()
 
     context = {
-        "form": my_form
+        "form": form
     }
     return render(request, "users/user_create.html", context)
 
 def user_login_view(request):
-    my_form = UserLoginForm()
-    if request.method == "POST":
-        my_form = UserLoginForm(request.POST)
-        if my_form.is_valid():
-            if User.objects.filter(username=my_form.cleaned_data['username']).exists():
-                if User.objects.filter(username=my_form.cleaned_data['username'], password=my_form.cleaned_data['password']).exists():
-                    return HttpResponseRedirect('/')
-                else:
-                    messages.add_message(request, messages.ERROR, "Contraseña incorrecta.")
-            else:
-                messages.add_message(request, messages.ERROR, "Usuario incorrecto.")
-
-    context = {
-        "form": my_form
-    }
-    return render(request, "users/user_login.html", context)
+    if user is not None:
+        login(request, user)
+        return redirect('/')
+    else:
+        messages.add_message(request, messages.ERROR, "Datos incorrectos.")
