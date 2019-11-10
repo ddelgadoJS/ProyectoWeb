3155e153f6a547f44e27885eda0a97e9efd2815c
Daniel Delgado <delgado.jdds@gmail.com>
2019-11-10 15:22:51 -0600

    Agregar y modificar paradas listo

--- venv/src/pages/views.py
+++ venv/src/pages/views.py
@@ -6,7 +6,7 @@ from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-from .models import Empresa, Ruta
+from .models import Empresa, Ruta, Parada
 from .forms import *
 from .lib import crear_log
 
@@ -169,6 +169,67 @@ def ruta_modify_view(request, *args, **kwargs):
     }
     
     return render(request, "rutas/rutas_modificar.html", context)
+
+@login_required
+def parada_view(request, *args, **kwargs):
+    registered_routes_queryset = Ruta.objects.all()
+
+    ruta_id = request.GET.get('id')
+    # Default company to show routes.
+    if ruta_id is None: ruta_id = registered_routes_queryset[0].id
+
+    routes_stops = Parada.objects.filter(ruta=ruta_id)
+
+    context = {
+        "registered_routes": registered_routes_queryset,
+        "routes_stops": routes_stops,
+        "ruta_id": int(ruta_id),
+    }
+
+    return render(request, "paradas/paradas_home.html", context)
+
+@login_required
+def parada_create_view(request, *args, **kwargs):
+    form = ParadaCreateForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('/paradas')
+
+    context = {
+        'form': form
+    }
+    
+    return render(request, "paradas/paradas_registrar.html", context)
+
+@login_required
+def parada_modify_view(request, *args, **kwargs):
+    parada_id = request.GET.get('id')
+    parada = Parada.objects.get(id=parada_id)
+    data = {
+        'ruta': parada.ruta,
+        'nombre': parada.nombre,
+        'description': parada.description,
+        'horario': parada.horario,
+        'latitud': parada.latitud,
+        'longitud': parada.longitud
+    }
+
+    form = ParadaUpdateForm(initial=data)
+    if request.method == 'POST':
+        if 'update_button' in request.POST:
+            form = ParadaUpdateForm(request.POST, instance=parada)
+            if form.is_valid():
+                form.save()
+                return redirect('/paradas')
+        elif 'delete_button' in request.POST:
+            Parada.objects.get(id=parada_id).delete()
+            return redirect('/paradas')
+
+    context = {
+        'form': form
+    }
+    
+    return render(request, "paradas/paradas_modificar.html", context)
 
 @login_required
 def profile_view(request, *args, **kwargs):
